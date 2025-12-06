acfb3c08caca17eacf94ca4bb73846fb408bb09a
NEXINEW <49754614+NEXINEW@users.noreply.github.com>
2025-12-06 13:18:55 +0800

    Update Switch3.vue
    这个比上一个的好处是 
    计算不算复杂
    简单直观
    当然也丰富了功能
    实现了背景颜色滑动

--- src/vue/0/Switch3.vue
+++ src/vue/0/Switch3.vue
@@ -1,3 +1,11 @@
+<!-- 
+这个比上一个的好处是 
+计算不算复杂
+简单直观
+当然也丰富了功能
+实现了背景颜色滑动
+-->
+
 <script setup lang="ts">
 import { useAttrs, computed, useTemplateRef, ref, onMounted, nextTick, onBeforeUnmount } from 'vue';
 import { twMerge } from 'tailwind-merge';
@@ -6,6 +14,7 @@ import { twMerge } from 'tailwind-merge';
 defineOptions({
     inheritAttrs: false
 })
+
 
 const attrs = useAttrs()
 
@@ -16,24 +25,48 @@ const filterAttrs = computed(() => {
 
 interface Props {
     active: boolean
-    gap?: number
+    trackBaseClass?: string
+    trackClass?: string
+    thumbClass?: string
 };
 
 const prop = withDefaults(defineProps<Props>(), {
-    gap: 0,
+    trackBaseClass: "",
+    trackClass: ""
 })
 
 const mergedRootClass = computed(() => {
     return twMerge(
-        "w-12 h-4 rounded-full cursor-pointer flex items-center transition-colors duration-200 ease-out m-20",
-        prop.active ? "bg-blue-400" : "bg-gray-400",
+        "w-12 h-4 rounded-full cursor-pointer bg-gray-400 m-20 flex relative",
         attrs.class as string || ""
+    )
+})
+
+const mergedTrackBaseClass = computed(() => {
+    let trackBaseWidth;
+    if (!prop.active) {
+        trackBaseWidth = 'w-0';
+    } else {
+        trackBaseWidth = 'w-full';
+    }
+    return twMerge(
+        'h-full rounded-full bg-blue-400 transition-all duration-200 ease-out rounded-full',
+        prop.trackBaseClass,
+        trackBaseWidth
+    );
+});
+
+const mergedTrackClass = computed(() => {
+    return twMerge(
+        "absolute top-1/2 transition-all duration-200 ease-out",
+        prop.trackClass
     )
 })
 
 const mergedThumbClass = computed(() => {
     return twMerge(
-        "aspect-square bg-white -translate-y-[50%] rounded-full transition-all duration-200 ease-out",
+        "absolute h-6 w-6 right-0 bg-white rounded-full translate-x-1/2 -translate-y-1/2",
+        prop.thumbClass
     )
 })
 
@@ -60,31 +93,37 @@ onBeforeUnmount(() => {
     }
 })
 
-const thumbHeight = computed(() => {
+const trackWidth = computed(() => {
     forceUpdateKey.value
-    if (!root.value) return 0
-    return root.value.offsetHeight - 2 * prop.gap
-})
-
-const translateX = computed(() => {
-    forceUpdateKey.value
-    if (!root.value) return 0
-    if (prop.active) {
-        const x = root.value.offsetWidth - root.value.offsetHeight + prop.gap
-        return x
-    } else {
-        const x = prop.gap
-        return x
+    if (root.value) {
+        const width = root.value.offsetWidth - root.value.offsetHeight
+        return width
     }
 })
 
+const placeholderLeft = computed(() => {
+    forceUpdateKey.value
+    if (root.value) {
+        const width = root.value.offsetHeight / 2
+        return width
+    }
+})
 </script>
-
 <template>
+    <!-- 这是组件 -->
     <div :class="mergedRootClass" v-bind="filterAttrs" ref="root">
-        <div class="w-full h-0 shrink-0">
-            <div :class="mergedThumbClass"
-                :style="{ transform: `translateX(${translateX}px)`, height: `${thumbHeight}px` }"></div>
+        <!-- 这是防止颜色溢出容器，即使rounded-full，当宽度到0的时候，会变成竖线，很显眼 -->
+        <div class="w-full h-full rounded-full overflow-hidden">
+            <!-- 这是背景 -->
+            <div :class="mergedTrackBaseClass">
+            </div>
+        </div>
+
+        <!-- 这是轨道 -->
+        <div :class="mergedTrackClass"
+            :style="{ left: `${placeholderLeft}px`, width: prop.active ? `${trackWidth}px` : '0' }">
+            <!-- 这是滑块 -->
+            <div :class="mergedThumbClass"></div>
         </div>
     </div>
 </template>
